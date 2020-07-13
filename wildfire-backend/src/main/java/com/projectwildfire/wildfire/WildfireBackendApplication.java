8e06824908fac58b62c62f701d685e7b1a5ea46b
BANIK <VISHAL.BANIK@finastra.com>
2020-07-13 21:20:27 +0530

    Initial commit

---
+++ wildfire-backend/src/main/java/com/projectwildfire/wildfire/WildfireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectwildfire.wildfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildfireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildfireBackendApplication.class, args);
+	}
+
+}
---
+++ wildfire-backend/src/test/java/com/projectwildfire/wildfire/WildfireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectwildfire.wildfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildfireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
